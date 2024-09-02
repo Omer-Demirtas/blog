aa442879850df8956d40ce415b34bfcef1af1aca
Omer-Demirtas <gamega55834@gmail.com>
2024-09-02 21:23:47 +0300

    Typographyh4  added

---
+++ components/common/TypographyH4.tsx
@@ -0,0 +1,10 @@
+
+const TypographyH4 = ({ children }: { children: String }) => {
+    return (
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+            {children}
+        </h4>
+    );
+}
+ 
+export default TypographyH4;
