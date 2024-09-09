185c1c44ee913554f60159af4a7a1700308e635f
Omer-Demirtas <gamega55834@gmail.com>
2024-09-09 21:09:59 +0300

    Navbar component added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NavBar from "@/components/NavBar";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,10 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavBar />
         <div className="container mx-auto px-4 py-4">
-        {children}
+          {children}
         </div>
-        </body>
+      </body>
     </html>
   );
 }
