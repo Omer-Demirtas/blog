9d959711654e34900c02a0d17d92fa4d1be10a7b
Omer-Demırtas <omer.halil.dmrts@gmail.com>
2025-03-09 03:54:36 +0300

    TypeWriter styles updated and init text added

--- components/common/TypeWriter.tsx
+++ components/common/TypeWriter.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 
-const TypeWriter = ({ fullText }: { fullText: string}) => {
+const TypeWriter = ({ fullText }: { fullText: string }) => {
   const [text, setText] = useState("");
   const [index, setIndex] = useState(0);
 
@@ -11,17 +11,17 @@ const TypeWriter = ({ fullText }: { fullText: string}) => {
       const timeout = setTimeout(() => {
         setText((prev) => prev + fullText.charAt(index));
         setIndex((prev) => prev + 1);
-      }, 100);
+      }, 50);
 
 
-      
+
       return () => clearTimeout(timeout);
     }
   }, [index, fullText]);
 
   return (
-    <h1 className="text-5xl font-extrabold">
-        {text}
+    <h1 className="text-5xl md:text-7xl text-left font-extrabold text-foreground font-mono tracking-tight">
+      {text === "" ? (fullText.charAt(0)) : text}
     </h1>
   );
 };
